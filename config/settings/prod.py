308044a8f71a18132001e93a83474fc7bd2876b2
Uchenna J. Onyenso <alphadev.onyenso@gmail.com>
2023-08-11 04:46:06 +0100

    patch add pyproject.toml

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -1,12 +1,13 @@
 from .base import *
 import dj_database_url
+from decouple import config
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
     'default': dj_database_url.config(
-        default="postgres://alphadev:0SM9vX5QU5xRc4Zhp4h4AWTdAPVj6bBd@dpg-cjaqiapitvpc73bj6hvg-a.oregon-postgres.render.com/quickcheck",
+        default=config("DATABASE_URL"),
         conn_max_age=600
     )
 }
